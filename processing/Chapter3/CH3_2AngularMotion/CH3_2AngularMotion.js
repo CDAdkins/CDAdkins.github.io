d2df88b0ae8eb58d14ae2a8f9ec67c9138969f79
chris <adkinscda@gmail.com>
2024-04-08 17:34:12 -0700

    Fixed rounding error on CH3_2 Acceleration

--- processing/Chapter3/CH3_2AngularMotion/CH3_2AngularMotion.js
+++ processing/Chapter3/CH3_2AngularMotion/CH3_2AngularMotion.js
@@ -22,7 +22,7 @@ function draw() {
 
 
   translate(width/2, height/2)
-  text("Acceleration: " + round(angleA*1000), -width/2.1, -height/2.1);
+  text("Acceleration: " + round(angleA*1000, 2), -width/2.1, -height/2.1);
   text("Velocity: " + round(angleV*1000), -width/2.1, -height/2.25);
   rotate(angle);
   rect(0, 0, width/3, width/50);
