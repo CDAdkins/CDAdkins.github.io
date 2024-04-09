1d3ead221184513900471c30e5ceb790b7a80802
chris <adkinscda@gmail.com>
2024-04-08 17:14:55 -0700

    Implemented CH3_2

--- processing/Chapter3/CH3_1Angles/CH3_1Angles.js
+++ processing/Chapter3/CH3_1Angles/CH3_1Angles.js
@@ -1,18 +1,30 @@
 let i;
+let reverse;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(24);
   i = 10;
+  reverse = false;
   textSize(24);
 }
 
 function draw() {
-  i *= 1.0015;
-  background(24, 24, 24, i);
+    if (i >= 100) {
+        reverse = true;
+    } else if (i <= 10) {
+        reverse = false;
+    }
+
+    if (reverse) {
+        i /= 1.0015;
+    } else {
+        i *= 1.0015;
+    }
+  background(24, 24, 24, i/4);
   noStroke();
   fill(240, 100, 160);
   rectMode(CENTER);
-  text("Rotating object at " + round(i) + " radians/frame", 10, 30);
+  text("Rotating object at " + round(i) + " radians/second", 10, 30);
 
 
   translate(width/2, height/2)
--- processing/Chapter3/CH3_2AngularMotion/CH3_2AngularMotion.js
+++ processing/Chapter3/CH3_2AngularMotion/CH3_2AngularMotion.js
@@ -1,3 +1,45 @@
+let angle = 0;
+let angleV = 0;
+let angleA = 0;
+
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
+  background(24);
+  textSize(24);
+}
+
+function draw() {
+  
+  angleV += angleA;
+  angle += angleV;
+
+  background(24);
+  noStroke();
+  fill(240, 100, 160);
+  rectMode(CENTER);
+  
+
+
+  translate(width/2, height/2)
+  text(angleA, -width/2.1, -height/2.1);
+  rotate(angle);
+  rect(0, 0, width/3, width/50);
+
+  if (keyIsDown(DOWN_ARROW) || keyIsDown(LEFT_ARROW)) {
+    angleA -= 0.000005;
+  } else if (keyIsDown(UP_ARROW) || keyIsDown(RIGHT_ARROW)) {
+    angleA += 0.000005;
+  } else {
+    if (angleV > 0) {
+        angleV -= 0.0002;
+        angleA = 0;
+    } else {
+        angleV += 0.0002;
+        angleA = 0;
+    }
+    
+  }
+
+
+
 }
