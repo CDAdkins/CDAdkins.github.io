66e9a1e6ad006c121158b460faf749d746983dcd
Chris Adkins <c.adkins@rocketmail.com>
2025-12-02 11:16:44 -0800

    Added testing link for testing things.

---
+++ ball_game/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2375689.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 265973057, 265973058, 265973056 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/265973075/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/265973079/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/265973070/1/ammo.js', 'preload' : true},
+];
